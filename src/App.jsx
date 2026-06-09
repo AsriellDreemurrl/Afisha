2c39ecf33734c3a7c040ae6819fae9a48a9585bd
AsriellDreemurrl <bkgjbjkntbjitbnj@gmail.com>
2026-06-09 16:07:46 +0600

    импортированы страницы, установлен git-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Editor from './pages/Editor/Editor'
+import Post from './pages/Post/Post'
 
 
 function App() {
