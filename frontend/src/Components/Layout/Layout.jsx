108b292d562a599d5d14bb0c9d5e87a84234ab76
AsriellDreemurrl <bkgjbjkntbjitbnj@gmail.com>
2026-06-10 15:18:08 +0600

    fixed auto-restart issue due to capital Components letter

--- frontend/src/Components/Layout/Layout.jsx
+++ frontend/src/Components/Layout/Layout.jsx
@@ -3,7 +3,6 @@ import './Layout.css'
 function Layout({ children }) {
   return (
     <div className="layout">
-        <h1>j1212weffwef</h1>
         {children}
     </div>
   )
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@
 function Home() {
   return (
     <div className="mainpage">
-        Hello world
+        Hello worldeee
     </div>
   )
 }
