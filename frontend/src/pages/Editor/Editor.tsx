189943d0caeaa76fee823c5a81aa09e432ffb654
DuishonalievBekbolot <dujsonalievicb@gmail.com>
2026-06-11 11:15:14 +0600

    feat: страница редактирования и создания

--- frontend/src/pages/Editor/Editor.tsx
+++ frontend/src/pages/Editor/Editor.tsx
@@ -1,6 +1,49 @@
 // Editor.jsx
+import style from "./Editor.module.css"
 function Editor() {
-  return <div>Editor</div>
+  return (
+    <div className={style.container}>
+      <h1 className={style.name}>Новое событие</h1>
+      <label htmlFor="title" className={style.label}>Название</label>
+      <input type="text" className={style.title} id="title" />
+      <label htmlFor="desc" className={style.label}>Описание</label>
+      <textarea className={style.description} id="desc" />
+      <div className={style.aboutinp_wrapper}>
+        <div className={style.input_group}>
+          <label htmlFor="date" className={style.label}>Дата и время</label>
+          <input type="datetime-local" className={style.aboutinp} id="date" />
+        </div>
+        <div className={style.input_group}>
+          <label htmlFor="place" className={style.label}>Место</label>
+          <input type="text" className={style.aboutinp} id="place" />
+        </div>
+      </div>
+      <div className={style.aboutinp_wrapper}>
+        <div className={style.input_group}>
+          <label htmlFor="cat" className={style.label}>Категория</label>
+          <select className={style.aboutinp} id="cat"  >
+            <option value="" disabled selected hidden>Выберите</option> 
+            <option value="Концерт">Концерт</option>
+            <option value="Лекция">Лекция</option>
+            <option value="Спорт">Спорт</option>
+            
+            </select>
+        </div>
+        <div className={style.input_group}>
+          <label htmlFor="price" className={style.label}>Цена</label>
+          <input type="text" className={style.aboutinp} id="price" />
+        </div>
+      </div>
+        <label htmlFor="photo" className={style.label}>Ссылка на фото</label>
+        <input type="url" className={style.photo} id="photo" />
+      <div className={style.btnwrapper}>
+        <button className={style.btn}>Сохранить</button>
+        <button className={style.btn}>Отмена</button>
+      </div>
+
+      
+    </div>
+  )
 }
 
 export default Editor
