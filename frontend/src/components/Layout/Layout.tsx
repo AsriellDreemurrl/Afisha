1d6bea86677c974bf57d3183dfa946a731a5d7c4
Albina Kostikova <albucit33@gmail.com>
2026-07-08 20:49:26 +0300

    added AppContext with UseContext for Header, added header into Layout and wrapped the site in AppProvider

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
+import Header from '../Header/Header'
 import styles from './Layout.module.css'
 import type { ReactNode } from 'react'
 
 const Layout = ({ children }: { children: ReactNode}) => {
   return (
     <div className={styles.layout}>
+      <Header />
         {children}
     </div>
   )
